1220d372db950e0103d188d03c7faaadaed81888
KYB <kyb031700@naver.com>
2022-03-27 22:26:10 +0900

    spot3/30

---
+++ main.js
@@ -0,0 +1,9 @@
+var setVisibility = {
+    showGame:function(self){
+        var timeAttack = document.getElementById("timeAttackWrapper");
+        var endless = document.getElementById("endlessWrapper");
+
+        timeAttack.style.visibility="visible";
+        endless.style.visibility="visible";
+    }
+}
